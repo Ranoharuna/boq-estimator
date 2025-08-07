79e038c894d829f6dc4175409907d2c55d435567
Haruna Abdullahi Yusuf <harunaayusufrano@yahoo.com>
2025-08-07 15:06:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,14 @@ function exportTableToExcel() {
   const wb = XLSX.utils.table_to_book(table, { sheet: "BOQ" });
   XLSX.writeFile(wb, "boq-estimate.xlsx");
 }
+function exportToPDF() {
+  const element = document.getElementById("boq-container");
+  const opt = {
+    margin:       0.5,
+    filename:     'boq-estimate.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+  };
+  html2pdf().set(opt).from(element).save();
+}
